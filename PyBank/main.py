f683fc753ac598fcce71aba3a6699ef4f23b2ce2
nj08831 <pa2237@yahoo.com>
2018-11-26 22:04:36 -0500

    Uploading code for Python-Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[33]:
+
 
 import os
 import csv
 
 bankdata = os.path.join('budget_data.csv')
-
 
 with open(bankdata, 'r') as bank_data:
     # Split the data on commas
@@ -14,6 +15,10 @@ with open(bankdata, 'r') as bank_data:
     header = next(csvreader)
     
     #print(header)
+    
+    from collections import OrderedDict
+    from statistics import mean
+    from datetime import datetime
     
     #define lists to store data for analyses
     month_year = []
@@ -29,9 +34,6 @@ with open(bankdata, 'r') as bank_data:
     #creates dictionary of key,value pairs to iterate thru
     #collections enables the data to stay ordered
     
-    from collections import OrderedDict
-    from statistics import mean
-    
     #create a list to find the $ change between month i and month i-1
     change = 0
     change_list=[]
@@ -45,17 +47,20 @@ with open(bankdata, 'r') as bank_data:
     avg_changes = sum(change_list)/len(change_list)
     
     # find the max and min amounts and their associated month-year    
-    final_stats= OrderedDict(zip(month_year,change_list))
+    final_stats= OrderedDict(zip(month_year[1:],change_list))
     
     max_key, max_value = max(final_stats.items(), key=lambda x:x[1])
     min_key, min_value = min(final_stats.items(), key=lambda x:x[1])
     
+    #Formatting the Date from MMM-YY to Month Year
+    max_key_for = datetime.strptime(max_key,'%b-%y').strftime('%B %Y')
+    min_key_for = datetime.strptime(min_key,'%b-%y').strftime('%B %Y')
     
     #format the values - use a list to iterate thru the values
     format_values = [total_profit_loss,avg_changes,max_value,min_value]
     formatted = []
-       
     
+     
     for value in format_values:
         if value >= 0:
             value = '${:,.0f}'.format(value) 
@@ -66,51 +71,51 @@ with open(bankdata, 'r') as bank_data:
             formatted.append(value)
             #print(value)
 
-    #OUTPUT RESULTS
-    #print (formatted[0])
     
-    print(f'Financial Analyses')
-    print(f'-----------------------------')
-    print(f'            ')
+#OUTPUT RESULTS
+    #print (formatted[0])
+    output = (
+    f'Financial Analyses \n'
+    f'----------------------------- \n'
+    f'          \n'
    
     #Counts unique months in the file
-    print(f'Total Months: {len(set(month_year))}')
+    f'Total Months: {len(set(month_year))} \n'
     
     #print Total Amount over all Months
-    print(f'Total Net Amount (Profit/Loss): {formatted[0]}')
+    f'Total Net Amount (Profit/Loss): {formatted[0]} \n'
                     
     #print Average Month over Month Change
-    print(f'Average Month over Month change (Profit/Loss) is: {formatted[1]}')
+    f'Average Month over Month change (Profit/Loss) is: {formatted[1]} \n'
     
     #print(f'Avg Mom change is: {mean(change_list)}')
           
     #print Max Profit and Month
-    print(f'Greatest Increase in Profits occurred {max_key} in the amount of:  {formatted[2]}' )
+    #f'{datetime.strptime(max_key,'%b-%y').strftime('%b %Y')} \n'
+    
+    f'Greatest Increase in Profits occurred {max_key_for} in the amount of:  {formatted[2]} \n'
     
     #print Min Profit and Month    
-    print(f'Greatest Decrease in Profits occurred {min_key} in the amount of: {formatted[3]}' )
+    f'Greatest Decrease in Profits occurred {min_key_for} in the amount of: {formatted[3]} \n'
+        
+    f'                \n'
+    f'                \n'
+    f'                \n'
+
+    f'Source: /PyBank/main.py \n'
+    f'Owner:  Dee Ann Belsky \n' 
+    )
     
-#Create an output text file
-# write data in a file. 
-output_file = open("PyBank_Results.txt","w") 
+    
+# Print to terminal
+    print(output)
+    
 
-output_file.write(f'                \n')
-output_file.write(f'Financial Analyses \n')
-output_file.write(f'-----------------------------')
-output_file.write(f'            \n')
-output_file.write(f'Total Months: {len(set(month_year))} \n')
-output_file.write(f'Total Net Amount (Profit/Loss): {formatted[0]} \n')
-output_file.write(f'Average Month over Month change (Profit/Loss) is: {formatted[1]} \n')
-output_file.write(f'Greatest Increase in Profits occurred {max_key} in the amount of:  {formatted[2]} \n'  )
-output_file.write(f'Greatest Decrease in Profits occurred {min_key} in the amount of: {formatted[3]} \n' )
-
-output_file.write(f'                \n')
-output_file.write(f'                \n')
-output_file.write(f'                \n')
-
-output_file.write(f'source:     PyBank.py \n')
-output_file.write(f'programmer: Dee Ann Belsky \n' )
-output_file.close() #to change file access modes 
+# Save the results to text file
+    with open("PyBank_Results.txt", "w") as txt_file:
+        txt_file.write(output)
+        txt_file.close()
+    
 
 
 #NOTES ONLY
@@ -122,5 +127,33 @@ output_file.close() #to change file access modes
  #['apple','ball']
  #d.items() # displays pair tuple of key and value
  #[('a','apple'),('b','ball')
+ #oldformat = datetime.strptime(max_key,'%b-%y')
+ #newformat = oldformat.strftime('%B %Y')
     
         
+   
+  
+    
+         
+        
+        
+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
